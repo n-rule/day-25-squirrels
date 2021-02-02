fb94440d4add2d778c6b9a16befd45bfbee0381e
n-rule <rule@ukr.net>
2021-02-02 11:33:44 +0200

    1

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
 import pandas
+
+data = pandas.read_csv('2018_Central_Park_Squirrel_Census_-_Squirrel_Data.csv')
+
+
+count = data['Primary Fur Color'].value_counts()
+
+print(count)
+
+squirrels_count = pandas.DataFrame(count)
+squirrels_count.to_csv('squirrel_count.csv')
+print('\n\n')
+
+print(squirrels_count)
